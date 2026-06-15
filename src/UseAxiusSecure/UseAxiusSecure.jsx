0cc4f90ce17b24a24be54adc4d68eb7004887742
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-06-15 16:16:36 +0600

    resolve

--- src/UseAxiusSecure/UseAxiusSecure.jsx
+++ src/UseAxiusSecure/UseAxiusSecure.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiusSecure = axios.create({
-  baseURL: "https://job-portal-servers-xi.vercel.app/",
+  baseURL: "https://job-portal-servers-xi.vercel.app",
 });
 const UseAxiusSecure = () => {
   return axiusSecure;
